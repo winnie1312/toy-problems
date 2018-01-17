fef6cd493b0bceebe526a4361f619d7de9b4cf56
Winnie Phan <nhuphan0404@gmail.com>
2018-01-17 09:36:08 +0700

    fix divisionStringified.js

--- divisonStringified.js
+++ divisonStringified.js
@@ -1,6 +1,26 @@
+//c1
 function divisionStringified(num1, num2){
-    let result = num1/ num2;
-    let check = num1 % num2;
-    return check === 0? result.toString() : Math.ceil(result).toString();
+    let finalNum = num1/ num2;
+    let whole = Math.round(finalNum).toString();
+    
+    let result = whole;
+    if(whole.length > 3){
+        let arr = whole.split("");
+        for(let i= whole.length-3; i>0; i-=3){
+            arr.splice(i,0,",");
+        }
+        result = arr.join("");
+    }
+    return result;
 };
-console.log(divisionStringified(175,24));
+console.log(divisionStringified(532222,2));
+
+//c2
+// function divisionStringified(num1, num2){
+//     let finalNum = num1/ num2;
+//     let result = Math.round(finalNum);
+//     return result.toLocaleString();
+// };
+// console.log(divisionStringified(50333,2));
+
+
