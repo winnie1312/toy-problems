d15aa1dc097981ceff3dc944d9ec83c9c23f21a8
Winnie Phan <nhuphan0404@gmail.com>
2018-01-29 22:13:04 +0700

    check cz

--- alphabet-soup.js
+++ alphabet-soup.js
@@ -2,8 +2,8 @@
 //number , punctuation aren't included in String -> regex
 
 //c1
-let AlphabetSoup =(str)=> {
-  let arr =str.match(/[a-z]/gi);
+function AlphabetSoup (str) {
+  let arr = str.match(/[a-z]/gi);
   console.log(arr);
   //c1: let sorted = arr.sort((a,b) => a.charCodeAt() - b.charCodeAt());
   let sorted = arr.sort(); //c2 (shorter than the above one)// array.sort() default order to Unicode
